9e60ba3b72573ee1b0de3f1c2a922ea262fe07be
James <jamesezhang@tencent.com>
2024-07-12 21:09:53 +0800

    feat: add google login

--- meeting-room-booking-system/meeting-room-booking-system-frontend-user/src/views/index/Index.tsx
+++ meeting-room-booking-system/meeting-room-booking-system-frontend-user/src/views/index/Index.tsx
@@ -1,15 +1,33 @@
 import { UserOutlined } from "@ant-design/icons";
 import { Outlet, Link } from "react-router-dom";
+import cookies from 'js-cookies';
 import './index.css';
+import { useEffect } from "react";
 
 export function Index() {
-    return <div id="index-container">
-        <div className="header">
-            <h1>会议室预定系统</h1>
-            <Link to={"/update-info"}><UserOutlined className="icon"/></Link>
-        </div>
-        <div className="body">
-            <Outlet></Outlet>
-        </div>
+  useEffect(() => {
+    const userInfo = cookies.get('userInfo');
+    const accessToken = cookies.get('accessToken');
+    const refreshToken = cookies.get('refreshToken');
+
+    if (userInfo && accessToken && refreshToken) {
+      localStorage.setItem('user_info', userInfo);
+      localStorage.setItem('access_token', accessToken);
+      localStorage.setItem('refresh_token', refreshToken);
+
+      cookies.remove('userInfo');
+      cookies.remove('accessToken');
+      cookies.remove('refreshToken');
+    }
+  }, []);
+
+  return <div id="index-container">
+    <div className="header">
+      <h1>会议室预定系统</h1>
+      <Link to={"/update-info"}><UserOutlined className="icon" /></Link>
     </div>
+    <div className="body">
+      <Outlet></Outlet>
+    </div>
+  </div>
 }
